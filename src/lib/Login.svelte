f5f476f445a8d4afc8fcd50e70460b19e4c68ace
Vlad Onis <vladonis.biz+opensource@gmail.com>
2023-08-25 20:34:21 +0300

    Initial commit

---
+++ src/lib/Login.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { invoke } from '@tauri-apps/api'
+
+    let username = '';
+    let password = '';
+
+    async function login() {
+        console.log(username);
+        console.log(password);
+    }
+</script>
+
+
+<form on:submit|preventDefault={login}>
+    <label>
+      Username:
+      <input type="text" bind:value={username} />
+    </label>
+    <label>
+      Password:
+      <input type="password" bind:value={password} />
+    </label>
+    <button type="submit">Login</button>
+  </form>
+  
+  <style>
+    form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 1rem;
+      max-width: 300px;
+      margin: 0 auto;
+    }
+  </style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Login from "$lib/Login.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<Login />
+
